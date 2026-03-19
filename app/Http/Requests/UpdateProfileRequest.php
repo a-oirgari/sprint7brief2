9c5f3419f2912c7f5654e6912b47e8851a5342b2
a-oirgari <abdrahmanghali1998@gmail.com>
2026-03-19 14:27:30 +0000

    chore: the updateProfile form request

---
+++ app/Http/Requests/UpdateProfileRequest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Requests\User;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateProfileRequest extends FormRequest
+{
+    public function authorize(): bool { return true; }
+
+    public function rules(): array
+    {
+        return [
+            'first_name' => 'sometimes|string|max:100',
+            'last_name'  => 'sometimes|string|max:100',
+            'email'      => 'sometimes|email|unique:users,email,' . auth()->id(),
+        ];
+    }
+}
