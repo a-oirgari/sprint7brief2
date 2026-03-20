7702c9ec04cdedb348cc37761b0641803d543b48
a-oirgari <abdrahmanghali1998@gmail.com>
2026-03-20 23:38:21 +0000

    chore: fixing the /bootstrap/app.php to feat

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,18 +1,41 @@
 <?php
-
+use App\Http\Middleware\AdminMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Auth\AuthenticationException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php', 
+        apiPrefix: 'api',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
+    ->withMiddleware(function (Middleware $middleware) {
+        
+        $middleware->alias([
+            'admin' => AdminMiddleware::class,
+        ]);
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
-        //
-    })->create();
+    ->withExceptions(function (Exceptions $exceptions) {
+        
+        $exceptions->render(function (AuthenticationException $e, $request) {
+            if ($request->expectsJson() || $request->is('api/*')) {
+                return response()->json([
+                    'message' => 'Non authentifié. Veuillez vous connecter.',
+                ], 401);
+            }
+        });
+
+        
+        $exceptions->render(function (ValidationException $e, $request) {
+            if ($request->expectsJson() || $request->is('api/*')) {
+                return response()->json([
+                    'message' => 'Erreur de validation.',
+                    'errors'  => $e->errors(),
+                ], 422);
+            }
+        });
+    })
+    ->create();
