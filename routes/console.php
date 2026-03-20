99878d61866c48016fd254a70cd3f98d311656f3
a-oirgari <abdrahmanghali1998@gmail.com>
2026-03-20 23:40:13 +0000

    chore: the routes/console.php to feat the scheduler

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,19 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+use App\Services\ScheduledBankingService;
+use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+Schedule::call(function () {
+    app(ScheduledBankingService::class)->chargeMonthlyFees();
+})
+    ->monthlyOn(1, '00:01')
+    ->name('monthly-fees')
+    ->withoutOverlapping();
+
+
+Schedule::call(function () {
+    app(ScheduledBankingService::class)->creditMonthlyInterests();
+})
+    ->monthlyOn(1, '00:05')
+    ->name('monthly-interests')
+    ->withoutOverlapping();
